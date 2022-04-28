89a813a366adec197bd1c86de4ecd51b10cc512d
Bharath Babu <66842073+bharathbabu68@users.noreply.github.com>
2022-04-28 11:09:55 +0530

    Added resources

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from './components/Main';
+import { ChakraProvider } from '@chakra-ui/react'
+
+function App() {
+  return (
+      
+    <Main />
+  
+  );
+}
+
+export default App;
