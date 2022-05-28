fdac19f947273131389016fc952cb60d9e50c5b4
Bharath Babu <66842073+bharathbabu68@users.noreply.github.com>
2022-05-28 19:14:09 +0530

    Update address.js

--- frontend/src/components/address.js
+++ frontend/src/components/address.js
@@ -1,6 +1,6 @@
 var addresses=
     {
-        ridebooking_contract_address: "0x6e9bBFF00DA4843bcEcc7671535Fd872C87f23f2",
+        ridebooking_contract_address: "0xEf91e0AF8a3D863e7BA203327b003e074540f86d",
         DRHP_contract_address: "0x4f3AaE20de27808cBC4fab6f89a5E987422B72B7",
         verifier_contract_address: "0x949D1EC16b59749a7d1D886e837AdAf6C9Ab3055"
     }
