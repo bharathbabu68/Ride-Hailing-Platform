5dca58136c8a4f27878015849b30789f593e6888
suriyaa333 <tsuriyaa@gmail.com>
2022-05-28 19:12:08 +0530

    Update address.js

--- frontend/src/components/address.js
+++ frontend/src/components/address.js
@@ -1,7 +1,7 @@
 var addresses=
     {
         ridebooking_contract_address: "0x6e9bBFF00DA4843bcEcc7671535Fd872C87f23f2",
-        DRHP_contract_address: "0x7A785Efa09aF769CAa680CEcB41610763944158B",
+        DRHP_contract_address: "0x4f3AaE20de27808cBC4fab6f89a5E987422B72B7",
         verifier_contract_address: "0x949D1EC16b59749a7d1D886e837AdAf6C9Ab3055"
     }
 
